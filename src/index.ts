65471026c83b0dd959a65fb54654dcf0731b0314
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-11-07 11:07:52 -0300

    feat: prisma create many

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { StoreServices } from "./services/store.services";
 const storeServices = new StoreServices();
 
 async function exampleController(){
-    const response = await storeServices.create("Alex Store");
+    const response = await storeServices.createMany(["Otavio Store", "Osvaldo Store"]);
 
     console.log(response);
 }
--- src/services/store.services.ts
+++ src/services/store.services.ts
@@ -2,6 +2,12 @@ import { prisma } from "../database/prisma";
 
 export class StoreServices{
     async create(storeName: string){
-        return await prisma.store.create({ data: { name: storeName}});
+        return await prisma.store.create({ data: { name: storeName }});
+    }
+
+    async createMany(storeNames: string[]){
+        const stores = storeNames.map(storeName => ({ name: storeName }));
+        
+        return await prisma.store.createMany({ data: stores})
     }
 }
