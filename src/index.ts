cc367f48381f14c72f6401ac222b9504a06b6be5
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-11-07 15:24:35 -0300

    feat: pagination

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const storeServices = new StoreServices();
 const productServices = new ProductServices();
 
 async function exampleController() {
-   const response = await storeServices.findOne(6);
+   const response = await storeServices.findMany(undefined, 5, 3);
 
    console.log(response);
 }
