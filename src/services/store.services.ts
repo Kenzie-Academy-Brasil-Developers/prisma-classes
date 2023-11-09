1b99ebae2781af8f6645a520f123893c6eddb64f
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-11-09 15:19:34 -0300

    identation

--- src/services/store.services.ts
+++ src/services/store.services.ts
@@ -2,6 +2,6 @@ import { prisma } from "../database/prisma";
 
 export class StoreServices{
     async create(storeName: string){
-        return await prisma.store.create({ data: { name: storeName}});
+        return await prisma.store.create({ data: { name: storeName }});
     }
 }
